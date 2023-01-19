a161b150373cf310991b526c8679072ca4e3f302
Reagan <ojthecat127@gmail.com>
2023-01-18 22:36:52 -0600

    todo list

--- ToDos.py
+++ ToDos.py
@@ -1,9 +1,11 @@
 '''
 TODO: 1. Figure out how to have seperate server profiles
+    when updating new cards, add to main database and then have a function that goes through all cards in each server profile and checks for old cards and then only adds the new ones
 TODO: 2. Draw/claim cards
 TODO: 3. Already owned cards award points
 TODO: 4. Profile
 TODO: 5. View all cards
+    filter system?
 TODO: 6. Battle
 TODO: 7. Trade
 TODO: 8. Shop?
