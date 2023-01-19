92ed698a326f6294cf268376f8ee7fb9663e0733
Reagan <ojthecat127@gmail.com>
2023-01-18 21:08:55 -0600

    card info command

---
+++ card_info.py
@@ -0,0 +1,39 @@
+import discord
+import random
+import datetime
+from datetime import date
+import json
+from discord.ext import commands, tasks
+from random import randint
+
+
+
+async def get_card_info(ctx, name, file="cards.json"):
+    with open (file) as database:
+        data = json.load(database)
+        cards = data.keys()
+        for x in cards:
+            if name.capitalize() == x:
+                card = data.get(x)
+        name = name.capitalize()
+        identity = card[0]
+        rarity = card[1]
+        franchise = card[2]
+        species = card[3]
+        primary = card[4]
+        secondary = card[5]
+        image = card[6]
+        ownership = card[7]
+        value = card[8]
+        level = card[9]
+    card_embed = discord.Embed(title=f"{name}", description=f"Rarity: {rarity}\n"
+                f"Owner: {ownership}\n"
+                f"Level: {level}\n"
+                f"Value: {value}\n"
+                f"Franchise: {franchise}\n"
+                f"Secret Identity: {identity}\n"
+                f"Species: {species}\n"
+                f"Primary Attack: {primary}\n"
+                f"Secondary Attack: {secondary}", color=0x6622f0)
+    card_embed.set_image(url=f"{image}")
+    await ctx.send(embed=card_embed)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 from discord.ext import commands, tasks
 from random import randint
 import create_card as cc
-
+import card_info as ci
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,7 +25,9 @@ async def testing(ctx):
 async def createCard(ctx):
     await cc.newCardEmbed(ctx, client)
 
-
+@client.command()
+async def cardinfo(ctx, name):
+    await ci.get_card_info(ctx, name)
 
 
 
