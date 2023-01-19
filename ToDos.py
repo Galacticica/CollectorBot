ce5b4f289b5cff524bd129121e52e2dcc1dcad1b
Reagan <ojthecat127@gmail.com>
2023-01-18 22:00:14 -0600

    todo list

---
+++ ToDos.py
@@ -0,0 +1,11 @@
+'''
+TODO: 1. Figure out how to have seperate server profiles
+TODO: 2. Draw/claim cards
+TODO: 3. Already owned cards award points
+TODO: 4. Profile
+TODO: 5. View all cards
+TODO: 6. Battle
+TODO: 7. Trade
+TODO: 8. Shop?
+TODO: 9. Other things
+'''
