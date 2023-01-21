6e403ec8afb74af5cf8d3a123d28bd53522fb2f7
Reagan <ojthecat127@gmail.com>
2023-01-20 22:09:05 -0600

    server profiles add new cards command added

--- ToDos.py
+++ ToDos.py
@@ -1,6 +1,7 @@
 '''
 Done: 1. Figure out how to have seperate server profiles
     when updating new cards, add to main database and then have a function that goes through all cards in each server profile and checks for old cards and then only adds the new ones
+TODO: 1.5. Update card data across servers
 TODO: 2. Draw/claim cards
 TODO: 3. Already owned cards award points
 TODO: 4. Profile
